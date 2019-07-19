8aafbba23378965bb4a105aefb542c6e2158be18
calmwalter <calmwalter@outlook.com>
2019-07-19 12:08:06 +0800

    just make a little change

---
+++ tree.py
@@ -0,0 +1,50 @@
+class Node:
+    '''
+    the node to store the states and action Q value and the next step infomation
+    states store the node we have put chess    
+    actions contain the q value of action, achieved by dictionay
+    '''
+    def __init__(self,parent_node,state):
+
+        self.states = [x for x in parent_node.states]
+        self.states.append(state)
+        self.next_node = None
+        self.child_node = None
+        self.parent_node = parent_node
+        self.actions={}
+        #here I put the initial q value to the actions which doesn't contain any chess
+        states_len = len(self.states)
+        for i in range(9):
+            if i not in self.states:
+                self.actions[i]=0
+        
+        
+class Tree:
+    '''
+    this is the main class to store the data of the game 
+    '''
+    def __init__(self,head_node):
+        self.head_node = head_node
+        
+    def insert(self,parent_node,node):
+        '''
+        when i instert a node, i should:
+        first, check if the node already in the tree
+        second, find the last None node and put the new node to it
+        third, set the parent node and the child node
+        '''
+        if parent_node.child_node == None:
+            parent_node.child_node = node
+        else:
+            current_node = parent_node.next_node
+            flag = False
+            while current_node.next_node!=None:
+                if current_node.
+                current_node = current_node.next_node
+            current_node.next_node = node
+            
+    def find_node(self,node):
+        pass
+
+
+    
